3df2ab6fe4db949c9f75e0acf44111b15331587d
grantimusPrime <700gtkeller@gmail.com>
2020-12-01 18:30:33 -0700

    loading data from the full url instead of a relative path.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-loadData("/data/termsParty.csv").then((data) => {
+let fullURL =window.location.href;
+loadData(fullURL + "/data/termsParty.csv").then((data) => {
   console.log(data);
 
   const mountPoint = "partyTopTerms";
   const partyTopTerms = new PartyTopTerms(mountPoint, data);
 });
 
-loadData("/data/termsEngagement.csv").then((data) => {
+loadData(fullURL +"/data/termsEngagement.csv").then((data) => {
   console.log(data);
 
   const mountPoint = "engagementTerms";
   const engagementTerms = new EngagementTerms(mountPoint, data);
 });
 
-loadData("/data/Bioguide IDs.csv").then((bID) => {
-  loadData("/data/socialStats.csv").then((data) => {
+loadData(fullURL +"/data/Bioguide IDs.csv").then((bID) => {
+  loadData(fullURL +"/data/socialStats.csv").then((data) => {
     bioMap = bID.reduce((map, el) => {
       map[el.ID] = el.Name;
       return map;
